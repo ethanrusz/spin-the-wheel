412eac9b5d748e9e9fc21817466eebddd249bff6
Ethan Ruszanowski <ethanrusz@gmail.com>
2019-09-27 01:00:20 -0400

    added hotfix

--- spin.py
+++ spin.py
@@ -4,6 +4,6 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-while True
+while True:
     if GPIO.input(10) == GPIO.HIGH:
         print("Button pushed")
