b51f0c24f047533d361ad2cbdaaa2574d789ff74
Ethan Ruszanowski <ethanrusz@gmail.com>
2019-09-27 08:46:25 -0400

    Moved led vars to top

--- spin.py
+++ spin.py
@@ -3,12 +3,12 @@ import RPi.GPIO as GPIO
 # Set up GPIO
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(led)
-GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
 # Define button pins
 led = 12
 button = 16
+
+GPIO.setup(led)
+GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Turn LED on
 def ledOn():
