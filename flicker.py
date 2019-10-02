e7448e42c3597de948c3e7d64c964272b7f3c60f
Ethan Ruszanowski <ethanrusz@gmail.com>
2019-10-02 13:32:47 -0400

    Added an outline for hue control

---
+++ flicker.py
@@ -0,0 +1,24 @@
+from qhue import Bridge
+import time
+import random
+import sys
+
+bridge_ip = "<hue_ip_address>"
+username = "<username>"
+my_lights = [light_id_1, light_id_2]
+
+def flicker(bridge, elapsed_time, lights_list = [], transition = 10):
+    start_time= time.time()
+    while (time.time() - start_time < elapsed_time):
+        for light in lights_list:
+            bridge.lights[light].state(bri=random.randint(50,130),on=True)
+        time.sleep(random.uniform(0,0.5))
+        for light in lights_list:
+            bridge.lights[light].state(transitiontime=random.randint(0, transiti
+on), on=False)
+        time.sleep(random.uniform(0,0.5))
+
+b = Bridge(bridge_ip_address, username)
+flicker(b, float(sys.argv[1]), my_lights, transition = 1)
+for light in light_list:
+    b.lights[light].state(bri=255,on=True)
