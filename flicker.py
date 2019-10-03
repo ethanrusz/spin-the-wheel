2f0c8b67f6ff7568e2d26140090d1d2661b78b13
Ethan Ruszanowski <ethanrusz@gmail.com>
2019-10-03 19:09:32 -0400

    Adding random mode

--- flicker.py
+++ flicker.py
@@ -5,7 +5,7 @@ import sys
 
 bridge_ip = "<hue_ip_address>"
 username = "<username>"
-my_lights = [light_id_1, light_id_2]
+lights = [light_id_1, light_id_2]
 
 def flicker(bridge, elapsed_time, lights_list = [], transition = 10):
     start_time= time.time()
@@ -14,11 +14,10 @@ def flicker(bridge, elapsed_time, lights_list = [], transition = 10):
             bridge.lights[light].state(bri=random.randint(50,130),on=True)
         time.sleep(random.uniform(0,0.5))
         for light in lights_list:
-            bridge.lights[light].state(transitiontime=random.randint(0, transiti
-on), on=False)
+            bridge.lights[light].state(transitiontime=random.randint(0, transition), on=False)
         time.sleep(random.uniform(0,0.5))
 
 b = Bridge(bridge_ip_address, username)
-flicker(b, float(sys.argv[1]), my_lights, transition = 1)
+flicker(b, float(sys.argv[1]), lights, transition = 1)
 for light in light_list:
     b.lights[light].state(bri=255,on=True)
--- spin.py
+++ spin.py
@@ -1,67 +1,91 @@
 import RPi.GPIO as GPIO
 from time import sleep
 import os # You must manually install mpg123 first
+import random
 from multiprocessing import Process # Import process for threads
 import sys
+
+# Pin setup - Change this if your GPIO layout is different
+greenLEDPin = 16
+redLEDPin = 18
+doorPin = 22
+killPin = 15
+mutePin = 37
+randomPin = 13
 
 # Configure board and set warnings
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
 # LED output config
-GPIO.setup(16, GPIO.OUT, initial = GPIO.LOW) # Green LED
-GPIO.setup(18, GPIO.OUT, initial = GPIO.LOW) # Red LED
+GPIO.setup(greenLEDPin, GPIO.OUT, initial = GPIO.LOW) # Green LED
+GPIO.setup(redLEDPin, GPIO.OUT, initial = GPIO.LOW) # Red LED
 
 # Input config
-GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP) # Door Sensor
-GPIO.setup(15, GPIO.IN, pull_up_down = GPIO.PUD_UP) # Kill Button
-GPIO.setup(37, GPIO.IN, pull_up_down = GPIO.PUD_UP) # Mute Button
+GPIO.setup(doorPin, GPIO.IN, pull_up_down = GPIO.PUD_UP) # Door Sensor
+GPIO.setup(killPin, GPIO.IN, pull_up_down = GPIO.PUD_UP) # Kill Button
+GPIO.setup(mutePin, GPIO.IN, pull_up_down = GPIO.PUD_UP) # Mute Button
+GPIO.setup(randomPin, GPIO.IN, pull_up_down = GPIO.PUD_UP) # Toggle random mode Button
 
 # Flag to track audio
 flag = 1
+randomMode = False
 
 # Double blink green
 def green():
     for _ in range(2):
-        GPIO.output(16, GPIO.HIGH)
+        GPIO.output(greenLEDPin, GPIO.HIGH)
         sleep(1)
-        GPIO.output(16, GPIO.LOW)
+        GPIO.output(greenLEDPin, GPIO.LOW)
         sleep(1)
 
 # Double blink red
 def red():
     for _ in range(2):
-        GPIO.output(18, GPIO.HIGH)
+        GPIO.output(redLEDPin, GPIO.HIGH)
         sleep(.5)
-        GPIO.output(18, GPIO.LOW)
+        GPIO.output(redLEDPin, GPIO.LOW)
         sleep(.25)
 
-# Play rattle me bones audio file
+# Play audio file
 def bones():
-    audio = "bones.mp3"
+    if random == 1:
+        # audio = random.choice(os.listdir("./audio"))
+        print("Audio set to: " + audio)
+    else:
+        audio = "./audio/bones.mp3"
     os.system("mpg123 " + audio)
 
 # Strobe red and green, then exit
 def exit():
     for _ in range(20):
-        GPIO.output(18, GPIO.HIGH)
+        GPIO.output(redLEDPin, GPIO.HIGH)
         sleep(.075)
-        GPIO.output(18, GPIO.LOW)
+        GPIO.output(redLEDPin, GPIO.LOW)
         sleep(.075)
-        GPIO.output(16, GPIO.HIGH)
+        GPIO.output(greenLEDPin, GPIO.HIGH)
         sleep(.075)
-        GPIO.output(16, GPIO.LOW)
+        GPIO.output(greenLEDPin, GPIO.LOW)
         sleep(.075)
     sys.exit()
 
-# Disable rig for 15s
+# Disable rig for killPins
 def muted():
     flag = 2
-    GPIO.output(18, GPIO.HIGH)
-    sleep(15)
-    GPIO.output(18, GPIO.LOW)
+    GPIO.output(redLEDPin, GPIO.HIGH)
+    sleep(killPin)
+    GPIO.output(redLEDPin, GPIO.LOW)
     flag = 1
     green()
+
+# Toggle random mode
+def togRandom():
+    if randomMode False:
+        randomMode = True
+        print("Random Mode")
+    if randomMode True:
+        randomMode False
+        print("Classic Mode")
 
 # Confirm code is under main function
 if __name__ == "__main__":
@@ -70,9 +94,9 @@ if __name__ == "__main__":
         # Define door state and multiprocessing
         redPro = Process(target = red)
         bonesPro = Process(target = bones)
-        doorOpen = GPIO.input(22)
+        doorOpen = GPIO.input(doorPin)
         # Respond to door state
-        if doorOpen == False:
+        if doorOpen == False: # Door closed
             if flag == 1:
                 green()
                 flag = 0
@@ -87,10 +111,14 @@ if __name__ == "__main__":
                 bonesPro.join()
             flag = 1
 
-        kill = GPIO.input(15)
-        if kill == False: # Kill button is pushed
+        kill = GPIO.input(killPin)
+        if kill == False: # Kill button has been pushed
             exit()
 
-        mute = GPIO.input(37)
-        if mute == False: # mute has been pushed
+        mute = GPIO.input(mutePin)
+        if mute == False: # Mute button has been pushed
             muted()
+
+        random = GPIO.input(randomPin)
+        if random == False # Random mode button has been pushed
+            togRandom()
